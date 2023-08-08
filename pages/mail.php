7f51adef21bfee6b5c52cfa8f4b01a710da26d56
Adrian2C <adriancoceres.93@gmail.com>
2023-08-08 13:57:25 -0300

    personal portfolio

--- pages/mail.php
+++ pages/mail.php
@@ -1,16 +1,40 @@
 <?php
 
-// Get the form data
 $name = $_POST['name'];
-$phone = $_POST['phone'];
 $email = $_POST['email'];
+$subject = $_POST['subject'];
 $message = $_POST['message'];
 
-// Send an email
-$to = 'adriancoceres.93@gmail.com';
-$subject = 'Mensaje de Contacto ACWD';
-$body = "Nombre: $name\nEmail: $email\Telefono: $phone\nMensaje: \n $message";
+$mailheader = "From:".$name."<".$email.">\r\n";
 
-mail($to, $subject, $body);
-echo'mensaje enviado correctamente';
+$recipient = "adriancoceres.93@gmail.com";
+
+mail($recipient, $subject, $message, $mailheader) or die("Error!");
+
+echo'
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="../assets/favicon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="reset.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <div class="container">
+        <div class="container__background"></div>
+        <div class="container__text">
+        <h1>Gracias por contactarse! A la brevedad me contactare con usted!</h1>
+        <p class="back"><a href="../index.html">Volver a inicio</a></p>
+        </div>
+    </div>
+</body>
+
+</html>
+
+';
+
 ?>
